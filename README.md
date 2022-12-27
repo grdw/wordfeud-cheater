8a16c6d999c7d18b01faed4f7447bf126fbc7fd6
grdw <gerard@grdw.nl>
2022-12-27 15:56:21 +0100

    Add some initial code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Wordfeud cheater
 
 A little ditty that can figure out what the best words are to put to the table.
+
+## Setup:
+
+### Language: Dutch
+1. Download the latest wordlist from [Opentaal](https://github.com/OpenTaal/opentaal-wordlist)
+2. Add it under `data/nl/wordlist.txt`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let language = &args[1];
+    let base_path = format!("data/{}", language);
+
+    if !Path::new(&base_path).is_dir() {
+        panic!("Folder doesn't exist for language '{}'", language);
+    }
+
+    let letterpoints_file = format!("{}/letterpoints.json", base_path);
+    if !Path::new(&letterpoints_file).is_file() {
+        panic!(
+            "The 'letterpoints.json' file doesn't exist at '{}'",
+            letterpoints_file
+        );
+    }
+
+    let wordlist_file = format!("{}/wordlist.txt", base_path);
+    if !Path::new(&wordlist_file).is_file() {
+        panic!(
+            "The 'wordlist.txt' file doesn't exist at '{}'",
+            wordlist_file
+        );
+    }
+
+    println!("{:?}", args);
 }
